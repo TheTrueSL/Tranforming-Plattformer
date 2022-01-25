90b301ece4e9ce51e13e6e10888cc6cf44e5cf44
ge59zeg <ge59zeg@mytum.de>
2022-01-25 17:44:58 +0100

    Elephant can now move all objects which are tagged as Movable + have collider + rigidbody

---
+++ Assets/BastisScripts/Movable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+
+	[SerializeField] float power = 2f;	
+    
+	private void OnControllerColliderHit(ControllerColliderHit hit){
+		if(hit.gameObject.tag == "Movable"){
+			Rigidbody body = hit.gameObject.GetComponent<Rigidbody>();
+			Vector3 dir = hit.transform.position - transform.position;
+			dir = new Vector3(dir.x, 0, dir.z);
+			body.velocity = power*dir;
+		}
+	}
+}
