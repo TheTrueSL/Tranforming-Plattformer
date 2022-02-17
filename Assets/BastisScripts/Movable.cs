bb5737fa8a4322ae77c3aab55a18a89666d8bc5c
ge59zeg <ge59zeg@mytum.de>
2022-02-17 19:58:34 +0100

    Level für den Ochsen erstellt

---
+++ Assets/BastisScripts/Movable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+
+    [SerializeField] float power = 2f;    
+	private FirstPersonController fpc;
+
+	private void Start(){
+		fpc = gameObject.GetComponent<FirstPersonController>();
+	}
+    
+    private void OnControllerColliderHit(ControllerColliderHit hit){
+        if(fpc.currentForm == Form.Ox && hit.gameObject.tag == "Movable"){
+            Rigidbody body = hit.gameObject.GetComponent<Rigidbody>();
+            Vector3 dir = hit.transform.position - transform.position;
+            dir = new Vector3(dir.x, 0, dir.z);
+            body.velocity = power*dir;
+        }
+    }
+}
